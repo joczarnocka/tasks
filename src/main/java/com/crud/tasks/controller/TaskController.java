5cac18e4dee5f1246752b89adb8a4a7ac2e913de
jczarnocka@gmail.com <jczarnocka@gmail.com>
2018-10-20 21:05:26 +0200

    14.4

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/v1/task")
+@RestController
+public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks(){
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask/{taskId}")
+    public TaskDto getTask(@PathVariable(value="taskId")Long taskId){
+        return new TaskDto(1L,"test tile","test content");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask/{taskId}")
+    public void deleteTask(@PathVariable(value="taskId") Long taskId){
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto){
+        return new TaskDto(1L,"edited test tile","test content");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(TaskDto taskDto){
+    }
+
+}
+
