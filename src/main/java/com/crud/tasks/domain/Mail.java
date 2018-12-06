313840a49e12adbf7e8bc6d963ed63773377b9e6
jczarnocka@gmail.com <jczarnocka@gmail.com>
2018-12-06 20:51:06 +0100

    19.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Mail {
+
+ private String mailTo;
+ private String subject;
+ private String message;
+ private String toCc;
+
+}
